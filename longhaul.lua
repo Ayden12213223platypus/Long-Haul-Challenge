c85ee27bd7789d306b25632f58bb6bb0b3ab1dcc
Ayden <ayden12213223platypu@gmail.com>
2025-08-01 21:11:39 -0400

    Add files via upload

---
+++ longhaul.lua
@@ -0,0 +1,56 @@
+SMODS.Challenge{ -- Long Haul
+    loc_txt = "Long Haul",
+    key = 'longhaul',
+    rules = {
+        custom = {
+            {id = 'win_ante', value = 50}
+        },
+        modifiers = {
+        },
+    },
+    jokers = {
+    },
+    restrictions = {
+        banned_cards = {},
+        banned_tags = {},
+        banned_other = {}
+    },
+}
+
+SMODS.Challenge{ -- Longer Haul
+    loc_txt = "Longer Haul",
+    key = 'longerhaul',
+    rules = {
+        custom = {
+            {id = 'win_ante', value = 100}
+        },
+        modifiers = {
+        },
+    },
+    jokers = {
+    },
+    restrictions = {
+        banned_cards = {},
+        banned_tags = {},
+        banned_other = {}
+    },
+}
+
+SMODS.Challenge{ -- Longest Haul
+    loc_txt = "Longest Haul",
+    key = 'longesthaul',
+    rules = {
+        custom = {
+            {id = 'win_ante', value = 1000}
+        },
+        modifiers = {
+        },
+    },
+    jokers = {
+    },
+    restrictions = {
+        banned_cards = {},
+        banned_tags = {},
+        banned_other = {}
+    },
+}
